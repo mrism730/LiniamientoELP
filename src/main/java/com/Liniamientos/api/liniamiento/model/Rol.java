0c3e5dd5eb1001867fe27995ef37585288375631
josepandreee <70234554@elp.edu.pe>
2023-05-13 03:08:20 -0500

    modificar 3.1

--- src/main/java/com/Liniamientos/api/liniamiento/model/Rol.java
+++ src/main/java/com/Liniamientos/api/liniamiento/model/Rol.java
@@ -1,4 +1,19 @@
 package com.Liniamientos.api.liniamiento.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="rol")
 public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name ="Id")
+
+    private Integer id;
+
+    private String rol;
+
 }
